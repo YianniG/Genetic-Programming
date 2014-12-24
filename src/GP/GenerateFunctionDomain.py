cc327579af36d957e881ec3284d3b6d47773f3bf
YianniG <yianni000@hotmail.co.uk>
2014-12-24 00:05:01 +0000

    Init

---
+++ src/GP/GenerateFunctionDomain.py
@@ -0,0 +1,43 @@
+'''
+Created on 23 Dec 2014
+
+@author: Ioannis
+'''
+from operator import contains
+class GenerateFunctionDomain(object):
+    from random import Random;   
+    valueRange = [];
+    bannedValues = [];
+    
+    onlyIntegers = False;
+    useBanned = False;
+    useRange = False;
+    
+    rand = Random();
+
+    def __init__(self, vrange = [], bannedValues = [], onlyIntegers = False):
+        self.valueRange = vrange;
+        self.bannedValues = bannedValues;
+        self.onlyIntegers = onlyIntegers;
+        
+    def getRandomValue(self):
+        
+        if(len(self.valueRange) > 0):
+            #range is in use
+            self.useRange = True;
+        if(len(self.bannedValues) > 0):
+            #banned is in use
+            self.useBanned = True;
+    
+        return self.doWork();
+    
+    def doWork(self):
+        if(self.useRange and self.onlyIntegers):
+            value = self.rand.randrange(self.valueRange[0], self.valueRange[1])
+        
+        if(self.useRange): #Not just integers
+            value = self.rand.random() + self.rand.randint(self.valueRange[0], self.valueRange[1]-1);
+            
+        if(contains(self.bannedValues, value)):
+                return self.doWork();
+        return value;
---
+++ src/GP/__init__.py
@@ -0,0 +1,33 @@
+import math
+
+
+'''
+print();
+
+
+previousResults = [0];
+index = 0;
+base = 4;
+power = 0;
+
+f = open("O.txt", mode='w');
+
+while True:
+    try:
+        result = math.pow(base, power);
+        if(index==len(previousResults)):
+            previousResults.append(result);
+        elif(previousResults[index]<result):
+            previousResults[index] = result;
+        power=power+1;
+    except OverflowError:
+        print(base, " : ", power, " : ", previousResults[index]);
+        s = (base, " : ", power, " : ", previousResults[index])
+        f.write(str(s));
+        base=base+1;
+        power = 10
+    if base > 2000:
+        break;
+        
+f.close();'''
+    
